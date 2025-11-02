9fad5cead83e7fa6f32d80d9af9e37a60585ade6
Luciano Filho <lvgalvaofilho@gmail.com>
2025-11-02 15:27:59 -0300

    apply schema fixed

--- ingest_sql/bronze_to_silver/clean_claim.sql
+++ ingest_sql/bronze_to_silver/clean_claim.sql
@@ -4,10 +4,25 @@ CREATE OR REFRESH STREAMING TABLE smart_claims_dev.02_silver.claim (
 ) AS
 SELECT
   claim_no,
-  TO_DATE(claim_date)                            AS claim_date,
-  TO_DATE(incident_date)                         AS incident_date,
-  TO_DATE(license_issue_date, 'dd-MM-yyyy')      AS license_issue_date,
-  incident_hour,
-  *
-EXCEPT (claim_date, incident_date, license_issue_date)
+  CAST(policy_no AS BIGINT) AS policy_no,
+  TO_DATE(claim_date) AS claim_date,
+  CAST(months_as_customer AS INT) AS months_as_customer,
+  CAST(injury AS DOUBLE) AS injury,
+  CAST(property AS DOUBLE) AS property,
+  CAST(vehicle AS DOUBLE) AS vehicle,
+  CAST(total AS DOUBLE) AS total,
+  collision_type AS collision_type,
+  CAST(number_of_vehicles_involved AS INT) AS number_of_vehicles_involved,
+  CAST(age AS DOUBLE) AS age,
+  insured_relationship AS insured_relationship,
+  TO_DATE(license_issue_date, 'dd-MM-yyyy') AS license_issue_date,
+  TO_DATE(date) AS incident_date,
+  CAST(hour AS INT) AS incident_hour,
+  type AS incident_type,
+  severity AS severity,
+  CAST(number_of_witnesses AS INT) AS number_of_witnesses,
+  CAST(suspicious_activity AS BOOLEAN) AS suspicious_activity,
+  input_file,
+  input_file_mtime,
+  ingested_at
 FROM STREAM(smart_claims_dev.01_bronze.claim);
